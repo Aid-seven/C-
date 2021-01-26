15d9e90686cedd57999fd321ddc6cb2af1ec4ca8
Aid-seven <1578949298@qq.com>
2021-01-26 10:23:37 +0800

    学习

---
+++ 1.25/1.25/1.25.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+//int main()
+//{
+//	printf("一切都会非常好！\n");
+//	return 0;
+//}
+
+//int main()
+//{
+//	char ch = 'A';
+//	printf("%c\n", ch);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int age = 19;
+//	printf("%d\n", age);
+//	return 0;
+//}
+
+//int main() 
+//{
+//	long num = 100;
+//	printf("%d\n", num);
+//	return 0;
+//}
+
+int main()
+{
+	printf("%d\n", sizeof(char));
+	printf("%d\n", sizeof(short));
+	printf("%d\n", sizeof(int));
+	printf("%d\n", sizeof(long));
+	printf("%d\n", sizeof(long long));
+	printf("%d\n", sizeof(float));
+	printf("%d\n", sizeof(double));
+	return 0;
+}
