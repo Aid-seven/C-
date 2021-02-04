569678e92c57824177cd74f6e1f59de142df8551
Aid-seven <1578949298@qq.com>
2021-02-04 15:36:52 +0800

    学习

---
+++ 2.3/2.3/2.3.c
@@ -0,0 +1,106 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//int main()
+//{
+//	int n1 = 0;
+//	int n2 = 0;
+//	int sum = 0;
+//	scanf("%d%d", &n1, &n2);
+//	sum = n1 + n2;
+//	printf("sum=%d\n", sum);
+//	return 0;
+//}
+
+//int main()
+//{
+//	//const修饰的常变量
+//	//const——常属性
+//	
+//	const int num = 4;
+//	printf("%d\n", num);
+//	num = 8;
+//	printf("%d\n", num);
+//	//3;//直接写出一个数字——字面常量
+//	return 0;
+//}
+
+//int main()
+//{
+//	const int n = 10;
+//	int arr[n] = { 0 };
+//	//名为arr在十以内整型的数组
+//	n = 20;
+//	//两组错误表示n是变量，但又有常属性，所以是常变量
+//	return 0;
+//}
+
+//#define定义的标识符常量
+
+//#define MAX 10
+//int main()
+//{
+//	int arr[MAX] = {0};
+//	printf("%d\n", MAX);
+//	return 0;
+//}
+
+
+//枚举常量
+//枚举—一一列举
+//如，如三原色：红，黄，蓝
+//枚举关键字-enum
+//enum Sex
+//{
+//	MALE,
+//	FEMALE,
+//	SECRET
+//};
+//int main()
+//{
+//	enum Sex s = FEMALE;
+//	printf("%d\n", MALE);
+//	printf("%d\n", FEMALE);
+//	printf("%d\n", SECRET);
+//	return 0;
+//}
+
+//enum Color
+//{
+//	RED,
+//	BLUE,
+//	YELLOW
+//};
+//int main()
+//{
+//	printf("%d\n", RED);
+//	printf("%d\n", BLUE);
+//	printf("%d\n", YELLOW);
+//	return 0;
+//}
+
+//字符串类型
+
+//int main()
+//{
+//	char arr1[]="abc";//数组
+//	char arr2[] = { 'a' ,'b','c',0};
+//	printf("%s\n", arr1);
+//	printf("%s\n", arr2);
+//	return 0;
+//}
+//
+//int main()
+//{
+//	char arr1[] = "abc";
+//	char arr2[] = { 'a','b','c' };
+//	printf("%d\n", strlen(arr1));
+//	printf("%d\n", strlen(arr2));
+//	//strlen-string length 计算字符串长度
+//	return 0;
+//}
+
+int main()
+{
+	printf("d:\\初学c语言\\2.3\\2.3.c");
+	return 0;
+}
