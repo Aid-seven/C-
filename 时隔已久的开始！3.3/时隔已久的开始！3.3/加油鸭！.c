a270e6f3410e33788b63e4ee1779fd5f4263008a
Aid-seven <1578949298@qq.com>
2023-07-27 14:28:05 +0800

    1

---
+++ 时隔已久的开始！3.3/时隔已久的开始！3.3/加油鸭！.c
@@ -0,0 +1,40 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//int main()
+//{
+//	printf("Hello!Word!");
+//	return 0;
+//}
+
+//int main()
+//{
+//	char ch = 'a';
+//	printf("%d\n", ch);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int age = 20;
+//	printf("%d\n", age);
+//	return 0;
+//}
+
+//int main()
+//{
+//	long a = 100;
+//	printf("%d\n",a);
+//	return 0;
+//}
+
+int main()
+{
+	printf("%d\n", sizeof(char));
+	printf("%d\n", sizeof(int));
+	printf("%d\n", sizeof(short));
+	printf("%d\n", sizeof(long));
+	printf("%d\n", sizeof(float));
+	printf("%d\n", sizeof(double));
+	printf("%d\n", sizeof(long long ));
+	return 0;
+}
